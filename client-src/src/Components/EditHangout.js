5e3803ff7a2c01ebd1f4410e645a75d03d695533
camilo17 <cami172013@gmail.com>
2017-10-20 23:04:37 -0500

    Added functionality to EditHangout to have the current values in each input and the ability to change them.

---
+++ client-src/src/Components/EditHangout.js
@@ -0,0 +1,95 @@
+import React , {Component} from 'react';
+import axios from 'axios';
+import {Link} from 'react-router-dom'; 
+
+
+class EditHangout extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            name: '',
+            city: '',
+            address: ''
+        }
+
+        this.handleInputChange = this.handleInputChange.bind(this); 
+    }
+
+
+    async getHangoutDetails() {
+
+        const details = await axios.get(`http://localhost:3000/api/hangoutzs/${this.props.match.params.id}`);
+        this.setState({
+            name: details.data.name,
+            city: details.data.city,
+            address: details.data.address
+
+        })
+    }
+
+    componentWillMount(){
+        this.getHangoutDetails(); 
+    }
+
+    onSubmit(e) {
+
+        const updatedMeetup = {
+            name: this.refs.name.value,
+            city: this.refs.city.value,
+            address: this.refs.address.value
+        }
+    }
+
+    handleInputChange(e) {
+        
+
+        const target = e.target;
+        const value = target.value; 
+        const name = target.name; 
+
+        this.setState({
+            [name]: value
+        }); 
+
+
+    }
+    
+  
+
+  render(){
+    return (
+      <div>
+        <br/>  
+        <Link  className="btn grey"to="/">Back</Link>
+        <h1 className="center">Edit Hangout</h1>
+        <form onSubmit={this.onSubmit.bind(this)}>
+            <div className="input-field">
+                <input type="text" name="name" ref="name" value={this.state.name} onChange={this.handleInputChange}/>
+                <label htmlFor="name">Name</label>
+            </div>    
+            <div className="input-field">
+                <input type="text" name="city" ref="city" value={this.state.city} onChange={this.handleInputChange}/>
+                <label htmlFor="city">City</label>
+            </div>    
+            <div className="input-field">
+                <input type="text" name="address" ref="address" value={this.state.address} onChange={this.handleInputChange}/>
+                <label htmlFor="address">Address</label>
+            </div>    
+            <input type="submit" value="Save" className="btn"/>
+
+        </form>
+        
+
+      </div>
+    )
+  }
+
+
+}
+
+  
+
+
+export default EditHangout;
--- client-src/src/Components/Main.js
+++ client-src/src/Components/Main.js
@@ -5,6 +5,7 @@ import Hangoutz from './Hangoutz';
 import About from './About'; 
 import HangoutDetails from './HangoutDetails'; 
 import AddMeetUp from './AddMeetUp'; 
+import EditHangout from './EditHangout'; 
 
 const Main = () => (
     <main>
@@ -12,6 +13,7 @@ const Main = () => (
             <Route exact path="/" component={Hangoutz}/>
             <Route exact path="/about/:id" component={About}/>
             <Route exact path="/hangoutz/add" component={AddMeetUp}/>
+            <Route exact path="/hangoutz/edit/:id" component={EditHangout}/>
             <Route exact path="/hangoutz/:id" component={HangoutDetails}/>
         </Switch>    
 
